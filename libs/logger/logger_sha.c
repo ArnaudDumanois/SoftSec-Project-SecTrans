b707891f9cc6e73e06cd9eac7fa1b93aed16aae0
Julien D. <juliendidier83550@gmail.com>
2024-01-06 12:34:05 +0100

    login corrigé

--- libs/logger/logger_sha.c
+++ libs/logger/logger_sha.c
@@ -36,11 +36,11 @@ int save_user(const char *usrname, const char *passwd) {
     int file_descriptor = open(USERS_DB_FILE, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
 
     if (file_descriptor == -1) {
-        fprintf(stderr, "Erreur lors de l'ouverture du fichier des utilisateurs.\n");
+        perror("Erreur lors de l'ouverture du fichier des utilisateurs");
         exit(EXIT_FAILURE);
     }
 
-    if (user_exists(usrname, passwd) != -1) {
+    if (user_exists(usrname)) {
         printf("USER EXISTE !");
         close(file_descriptor);
         return -1;
@@ -52,10 +52,13 @@ int save_user(const char *usrname, const char *passwd) {
     hash_password(passwd, new_user.salt, new_user.hashed_password);
 
     if (write(file_descriptor, &new_user, sizeof(User)) != sizeof(User)) {
-        fprintf(stderr, "Erreur lors de l'écriture de l'utilisateur dans le fichier.\n");
+        perror("Erreur lors de l'écriture de l'utilisateur dans le fichier");
         close(file_descriptor);
         return -1;
     }
+
+    //Le compté est créé
+    //on créé un dossier
 
     close(file_descriptor);
     return 1;
@@ -74,11 +77,11 @@ int authenticate_user(const char *username, const char *password) {
     int authentication_result = 0;  // Par défaut, l'authentification échoue
 
     while ((read_result = read(file_descriptor, &user, sizeof(User))) == sizeof(User)) {
-        printf("%s\n%s\n",user.username,user.hashed_password);
+        printf("%s\n%s\n", user.username, user.hashed_password);
         if (strcmp(user.username, username) == 0) {
             char hashed_password[MAX_PASSWORD_LENGTH];
             hash_password(password, user.salt, hashed_password);
-            printf("HASHED PASSWD: %s\n",hashed_password);
+            printf("HASHED PASSWD: %s\n", hashed_password);
 
             if (strcmp(hashed_password, user.hashed_password) == 0) {
                 authentication_result = 1; // Authentification réussie
@@ -97,28 +100,31 @@ int authenticate_user(const char *username, const char *password) {
     return authentication_result;
 }
 
-int user_exists(const char *username, const char *password) {
-    FILE *file = fopen(USERS_DB_FILE, "rb");
-    if (!file) {
-        fprintf(stderr, "Erreur lors de l'ouverture du fichier des utilisateurs.\n");
+int user_exists(const char *username) {
+    int file_descriptor = open(USERS_DB_FILE, O_RDONLY);
+
+    if (file_descriptor == -1) {
+        perror("Erreur lors de l'ouverture du fichier des utilisateurs");
         exit(EXIT_FAILURE);
     }
 
     User user;
-    while (fread(&user, sizeof(User), 1, file) == 1) {
+    ssize_t read_result;
+    int user_exists = 0;  // Par défaut, l'utilisateur n'existe pas
+
+    while ((read_result = read(file_descriptor, &user, sizeof(User))) == sizeof(User)) {
         if (strcmp(user.username, username) == 0) {
-            char hashed_password[MAX_PASSWORD_LENGTH];
-            hash_password(password, user.salt, hashed_password);
-            if (strcmp(hashed_password, user.hashed_password) == 0) {
-                fclose(file);
-                return 1; // L'utilisateur existe et le mot de passe correspond
-            } else {
-                fclose(file);
-                return 0; // L'utilisateur existe, mais le mot de passe ne correspond pas
-            }
+            user_exists = 1;
+            break;  // Utilisateur trouvé
         }
     }
-    fclose(file);
-    return -1; // L'utilisateur n'existe pas
+
+    if (read_result == -1) {
+        perror("Erreur lors de la lecture du fichier des utilisateurs");
+        exit(EXIT_FAILURE);
+    }
+
+    close(file_descriptor);
+    return user_exists;
 }
 
